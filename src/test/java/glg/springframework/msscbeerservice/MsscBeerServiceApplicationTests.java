c29d32a6ba5a52286d19e4c9dfaede4bd328ac0a
1sNull <93542640+1sNull@users.noreply.github.com>
2022-03-22 22:40:37 -0400

    initial project add. Closes #1

---
+++ src/test/java/glg/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package glg.springframework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
